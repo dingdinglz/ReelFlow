fd3591ddccd4306bda19944a21dea69544a6d782
pyke <pykehuo@gmail.com>
2026-01-25 16:53:53 +0800

    feat(backend-rpc-etc): 添加数据库并创建实例

--- backend/rpc/user/internal/svc/servicecontext.go
+++ backend/rpc/user/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "ReelFlow/rpc/user/internal/config"
+import (
+	"ReelFlow/rpc/model/user"
+	"ReelFlow/rpc/user/internal/config"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	Model  user.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		Model:  user.NewUserModel(sqlx.NewMysql(c.DataSource), c.Cache),
 	}
 }
